7147d0b71e3fed6d11622c35bcfe623d46a54693
Jeremie Thibeault <jeremie.thibeault@gmail.com>
2020-06-09 13:55:48 -0400

    Adds the UI Tests header (working on Android)

---
+++ src/app/ApplicationTemplate.UITests/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uno.UITest.Helpers.Queries;
+
+namespace ApplicationTemplate.UITests
+{
+	public static class Constants
+	{
+		public const string WebAssemblyDefaultUri = "http://localhost:51669/";
+		public const string iOSAppName = "com.nventive.applicationtemplate";
+		public const string AndroidAppName = "com.nventive.applicationtemplate";
+		public const string iOSDeviceNameOrId = "00008030-000865912E90802E";
+
+		public static Platform PlatformUnderTest = Platform.Android;
+	}
+}
