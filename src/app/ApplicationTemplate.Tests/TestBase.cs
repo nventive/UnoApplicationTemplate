d3840fc676f7262c054aaa91a0272ff6168839b2
Jeremie Thibeault <jeremie.thibeault@gmail.com>
2020-10-01 12:17:55 -0400

    Adds support of mocked services in an integration test.

--- src/app/ApplicationTemplate.Tests/TestBase.cs
+++ src/app/ApplicationTemplate.Tests/TestBase.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Moq;
 using Nventive.Persistence;
 using Xunit;
 
@@ -15,6 +18,7 @@ namespace ApplicationTemplate.Tests
 	public class TestBase : IAsyncLifetime
 	{
 		protected static readonly CancellationToken DefaultCancellationToken = CancellationToken.None;
+		protected static readonly CancellationToken AnyCancellationToken = It.IsAny<CancellationToken>();
 
 		private readonly CoreStartup _coreStartup = new CoreStartup();
 
@@ -22,7 +26,7 @@ namespace ApplicationTemplate.Tests
 		{
 			_coreStartup.PreInitialize();
 
-			_coreStartup.Initialize();
+			_coreStartup.Initialize(ConfigureHost);
 
 			ConfigureSecurityProtocol();
 		}
@@ -32,9 +36,37 @@ namespace ApplicationTemplate.Tests
 			await _coreStartup.Start();
 		}
 
+		/// <summary>
+		/// A chance to configure the <paramref name="host"/> after its default configuration.
+		/// </summary>
+		/// <param name="host">Host builder</param>
+		protected virtual void ConfigureHost(IHostBuilder host) { }
+
+		/// <summary>
+		/// Returns the requested service.
+		/// </summary>
+		/// <typeparam name="TService">Type of service</typeparam>
+		/// <returns>Requested service</returns>
 		protected virtual TService GetService<TService>()
 		{
 			return _coreStartup.ServiceProvider.GetRequiredService<TService>();
+		}
+
+		/// <summary>
+		/// Replaces the registration for type <typeparamref name="TService"/> with a mocked implementation.
+		/// </summary>
+		/// <typeparam name="TService">Type of service</typeparam>
+		/// <param name="services">Service collection</param>
+		/// <param name="mockSetup">Mock configuration</param>
+		/// <returns>Services</returns>
+		protected virtual IServiceCollection ReplaceWithMock<TService>(IServiceCollection services, Action<Mock<TService>> mockSetup)
+			 where TService : class
+		{
+			var mockedService = new Mock<TService>();
+
+			mockSetup(mockedService);
+
+			return services.Replace(ServiceDescriptor.Singleton(mockedService.Object));
 		}
 
 		/// <summary>
