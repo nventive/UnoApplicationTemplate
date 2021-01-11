f7fedd2f3a08b23bd7526235a2eb3f3fdc51c456
Kevin Takla <kevin.takla@nventive.com>
2021-01-11 18:17:15 -0500

    test: fix favourite integration tests

--- src/app/ApplicationTemplate.Tests/Presentation/ChuckNorrisFavoritesPageViewModel.cs
+++ src/app/ApplicationTemplate.Tests/Presentation/ChuckNorrisFavoritesPageViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -14,29 +16,60 @@ namespace ApplicationTemplate.Tests
 		[Fact]
 		public async Task It_Should_Return_No_Results()
 		{
+			// Arrange
 			await StartNavigation(DefaultCancellationToken, () => new ChuckNorrisFavoritesPageViewModel());
 			var viewModel = (ChuckNorrisFavoritesPageViewModel)GetCurrentViewModel();
+
+			// Act
 			var quotes = await viewModel.Quotes.Load(DefaultCancellationToken) as ReadOnlyObservableCollection<ChuckNorrisItemViewModel>;
 
+			// Assert
 			quotes.Should().BeNullOrEmpty();
 		}
 
 		[Fact]
 		public async Task It_Should_Return_Favourited()
 		{
+			// Arrange
 			await StartNavigation(DefaultCancellationToken, () => new ChuckNorrisSearchPageViewModel());
 			var searchViewModel = (ChuckNorrisSearchPageViewModel)GetCurrentViewModel();
 			searchViewModel.SearchTerm = "dog";
 			var searchedQuotes = await searchViewModel.Quotes.Load(DefaultCancellationToken);
-			var firstQuote = searchedQuotes.First();
+			var firstQuoteVm = searchedQuotes.First();
+
+			// Act
+			await searchViewModel.ToggleIsFavorite.Execute(firstQuoteVm);
 
 			await StartNavigation(DefaultCancellationToken, () => new ChuckNorrisFavoritesPageViewModel());
 			var favouritesViewModel = (ChuckNorrisFavoritesPageViewModel)GetCurrentViewModel();
-			await favouritesViewModel.ToggleIsFavorite.Execute(firstQuote);
+
 			var favouritedQuotes = await favouritesViewModel.Quotes.Load(DefaultCancellationToken) as ReadOnlyObservableCollection<ChuckNorrisItemViewModel>;
 
+			// ReadOnlyObservableCollection does not load its content directly. Waiting for the CollectionChanged is a workaround to wait for its content to be loaded.
+			var tcs = new TaskCompletionSource<Unit>();
+			// CollectionChanged is private for ReadOnlyObservableCollection, therefore casting it into INotifyCollectionChanged is a workaround to be able to access this event
+			// https://stackoverflow.com/questions/2058176/why-is-readonlyobservablecollection-collectionchanged-not-public
+			((INotifyCollectionChanged)favouritedQuotes).CollectionChanged += OnFavouriteCollectionChanged;
+			await tcs.Task;
+			((INotifyCollectionChanged)favouritedQuotes).CollectionChanged -= OnFavouriteCollectionChanged;
+
+			// Assert
 			favouritedQuotes.Should().NotBeNull();
-			favouritedQuotes.Should().Contain(firstQuote);
+			var results = favouritedQuotes
+				.Select(vm => vm.Quote);
+			results.Should().Contain(firstQuoteVm.Quote);
+
+			void OnFavouriteCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+			{
+				try
+				{
+					tcs.TrySetResult(Unit.Default);
+				}
+				catch (Exception ex)
+				{
+					tcs.TrySetException(ex);
+				}
+			}
 		}
 	}
 }
