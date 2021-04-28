ec13dc74889f00f4bde857be58e4e198a237b7c7
Kevin Takla <kevin.takla@nventive.com>
2021-04-28 12:13:39 -0400

    test: add unit tests for OnDataLostDataLoaderStrategy

---
+++ src/app/ApplicationTemplate.Tests/Framework/DataLoader/OnDataLostDataLoaderStrategyShould.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Chinook.DataLoader;
+using FluentAssertions;
+using Xunit;
+
+namespace ApplicationTemplate.Tests.Framework
+{
+	public class OnDataLostDataLoaderStrategyShould
+	{
+		[Fact]
+		public async Task NotDisposePreviousData_When_FirstLoad()
+		{
+			// Arrange
+			var cpt = 0;
+			Action<object> mockAction = _ => cpt++;
+
+			var sut = new OnDataLostDataLoaderStrategy(mockAction);
+			sut.InnerStrategy = new MockDelegatingDataLoaderStrategy(() => Task.FromResult(new object()));
+
+			// Act
+			await sut.Load(CancellationToken.None, null);
+
+			// Assert
+			cpt.Should().Be(0);
+		}
+
+		[Fact]
+		public async Task DisposePreviousData_When_LoadNewReference()
+		{
+			// Arrange
+			var cpt = 0;
+			Action<object> mockAction = _ => cpt++;
+
+			var sut = new OnDataLostDataLoaderStrategy(mockAction);
+			sut.InnerStrategy = new MockDelegatingDataLoaderStrategy(() => Task.FromResult(new object()));
+			await sut.Load(CancellationToken.None, null);
+
+			// Act
+			await sut.Load(CancellationToken.None, null);
+
+			// Assert
+			cpt.Should().Be(1);
+		}
+
+		[Fact]
+		public async Task DisposePreviousData_When_LoadNewValue()
+		{
+			// Arrange
+			var cpt = 0;
+			Action<object> mockAction = _ => cpt++;
+
+			var value = new Random().Next(int.MaxValue);
+			var sut = new OnDataLostDataLoaderStrategy(mockAction);
+			sut.InnerStrategy = new MockDelegatingDataLoaderStrategy(async () => value);
+			await sut.Load(CancellationToken.None, null);
+
+			// Act
+			await sut.Load(CancellationToken.None, null);
+
+			// Assert
+			cpt.Should().Be(1);
+		}
+
+		[Fact]
+		public async Task NotDisposePreviousData_When_LoadSameReference()
+		{
+			// Arrange
+			var cpt = 0;
+			Action<object> mockAction = _ => cpt++;
+
+			var instance = new object();
+			var sut = new OnDataLostDataLoaderStrategy(mockAction);
+			sut.InnerStrategy = new MockDelegatingDataLoaderStrategy(() => Task.FromResult(instance));
+			await sut.Load(CancellationToken.None, null);
+
+			// Act
+			await sut.Load(CancellationToken.None, null);
+
+			// Assert
+			cpt.Should().Be(0);
+		}
+
+		public class MockDelegatingDataLoaderStrategy : DelegatingDataLoaderStrategy
+		{
+			private Func<Task<object>> _innerFunc;
+
+			public MockDelegatingDataLoaderStrategy(Func<Task<object>> innerFunc)
+			{
+				_innerFunc = innerFunc;
+			}
+
+			public override async Task<object> Load(CancellationToken ct, IDataLoaderRequest request)
+			{
+				return await _innerFunc();
+			}
+		}
+	}
+}
