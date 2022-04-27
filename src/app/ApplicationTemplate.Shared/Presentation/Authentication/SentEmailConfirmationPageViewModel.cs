c8be51f9164231293ddaaf74f0edd9a9d99774d8
Ariane Léonard <ariane.leonard@nventive.com>
2022-04-27 19:00:23 -0400

    Complete forgot password flow

--- src/app/ApplicationTemplate.Shared/Presentation/Authentication/SentEmailConfirmationPageViewModel.cs
+++ src/app/ApplicationTemplate.Shared/Presentation/Authentication/SentEmailConfirmationPageViewModel.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Chinook.DynamicMvvm;
+using Chinook.SectionsNavigation;
 
 namespace ApplicationTemplate.Presentation
 {
 	public class SentEmailConfirmationPageViewModel : ViewModel
 	{
+		public IDynamicCommand NavigateBackToLogin => this.GetCommandFromTask(async ct =>
+		{
+			await this.GetService<ISectionsNavigator>().RemovePrevious(ct);
+			await this.GetService<ISectionsNavigator>().NavigateBack(ct);
+		});
 	}
 }
