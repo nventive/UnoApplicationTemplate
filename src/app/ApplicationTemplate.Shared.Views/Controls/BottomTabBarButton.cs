f73c03512cf04a2b8da697e3913b3970993a9a6e
Gabriel Chouinard Létourneau <gabriel.chouinard@nventive.com>
2022-05-03 14:50:39 -0400

    refactor: remove internal logic and use Uno.Toolkit BottomTabBar / Keep BottomTabBarButton control to a minimum

---
+++ src/app/ApplicationTemplate.Shared.Views/Controls/BottomTabBarButton.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ApplicationTemplate.Views.Controls
+{
+	/// <summary>
+	/// Radio button with optional icon, used in the menu.
+	/// </summary>
+	public partial class BottomTabBarButton : Button
+	{
+		/// <summary>
+		/// Gets or sets the icon style on top of the RadioButton's text (Content).
+		/// </summary>
+		public Style IconStyle
+		{
+			get => (Style)GetValue(IconStyleProperty);
+			set => SetValue(IconStyleProperty, value);
+		}
+
+		public static readonly DependencyProperty IconStyleProperty =
+			DependencyProperty.Register("IconStyle", typeof(Style), typeof(BottomTabBarButton), new PropertyMetadata(default(Style)));
+	}
+}
