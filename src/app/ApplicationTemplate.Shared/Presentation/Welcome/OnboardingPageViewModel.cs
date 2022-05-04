f74f9dd41b42b9c2c258ae6a3ca301910e3c565b
Ariane Léonard <ariane.leonard@nventive.com>
2022-05-03 20:25:19 -0400

    Use IStringLocalizer for onboarding text

--- src/app/ApplicationTemplate.Shared/Presentation/Welcome/OnboardingPageViewModel.cs
+++ src/app/ApplicationTemplate.Shared/Presentation/Welcome/OnboardingPageViewModel.cs
@@ -4,31 +4,32 @@ using Chinook.DynamicMvvm;
 using Chinook.SectionsNavigation;
 using Chinook.StackNavigation;
 using Microsoft.Extensions.Localization;
-using ResourceLoader = Windows.ApplicationModel.Resources.ResourceLoader;
 
 namespace ApplicationTemplate.Presentation
 {
 	public partial class OnboardingPageViewModel : ViewModel
 	{
+		public OnboardingPageViewModel()
+		{
+			OnboardingItems = new OnboardingItemViewModel[]
+			{
+				new OnboardingItemViewModel(this.GetService<IStringLocalizer>()["Onboarding_Content"], "ms-appx:///Assets/Tutorial_FirstScreen_Icon.png"),
+				new OnboardingItemViewModel(this.GetService<IStringLocalizer>()["Onboarding_Content"], "ms-appx:///Assets/Tutorial_SecondScreen_Icon.png"),
+				new OnboardingItemViewModel(this.GetService<IStringLocalizer>()["Onboarding_Content"], "ms-appx:///Assets/Tutorial_ThirdScreen_Icon.png"),
+			};
+		}
+
 		public IDynamicCommand NavigateToLogin => this.GetCommandFromTask(async ct =>
 		{
 			await this.GetService<ISectionsNavigator>().NavigateAndClear(ct, () => new LoginPageViewModel(isFirstLogin: true));
 			await this.GetService<IApplicationSettingsService>().CompleteOnboarding(ct);
 		});
 
-		public OnboardingItemViewModel[] OnboardingItems { get; } = new OnboardingItemViewModel[]
+		public OnboardingItemViewModel[] OnboardingItems
 		{
-#if __ANDROID__ || __IOS__
-			new OnboardingItemViewModel(ResourceLoader.GetForViewIndependentUse().GetString("Onboarding_Content"), "ms-appx:///Assets/Tutorial_FirstScreen_Icon.png"),
-			new OnboardingItemViewModel(ResourceLoader.GetForViewIndependentUse().GetString("Onboarding_Content"), "ms-appx:///Assets/Tutorial_SecondScreen_Icon.png"),
-			new OnboardingItemViewModel(ResourceLoader.GetForViewIndependentUse().GetString("Onboarding_Content"), "ms-appx:///Assets/Tutorial_ThirdScreen_Icon.png"),
-#endif
-#if WINDOWS_UWP
-			new OnboardingItemViewModel("Get your shot of good humor everyday. Read the latest and greatest dad jokes!", "ms-appx:///Assets/Tutorial_FirstScreen_Icon.png"),
-			new OnboardingItemViewModel("Get your shot of good humor everyday. Read the latest and greatest dad jokes!", "ms-appx:///Assets/Tutorial_SecondScreen_Icon.png"),
-			new OnboardingItemViewModel("Get your shot of good humor everyday. Read the latest and greatest dad jokes!", "ms-appx:///Assets/Tutorial_ThirdScreen_Icon.png"),
-#endif
-		};
+			get => this.Get<OnboardingItemViewModel[]>();
+			set => this.Set(value);
+		}
 
 		public static implicit operator OnboardingPageViewModel(OnboardingItemViewModel v)
 		{
