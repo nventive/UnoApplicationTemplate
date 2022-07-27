cb68e34dc04090eb20ab97f6d90e138eddca45d0
jp.levesque <jean-philippe.levesque@nventive.com>
2022-07-27 14:50:00 -0400

    build: Add binlog

--- build/build-ios.sh
+++ build/build-ios.sh
@@ -5,4 +5,4 @@
 #$4 Provisioning Profile UUID
 #$5 Application identifier
 #$6 Package Version
-/Library/Frameworks/Mono.framework/Versions/Current/Commands/mono "/Applications/Visual Studio.app/Contents/MonoBundle/MSBuild/Current/bin/MSBuild.dll" /t:Build "$1" /p:Configuration="$2" /p:Platform="$3" /p:buildForSimulator=false /p:BuildIpa=true /p:CodesignProvision="$4" /p:ApplicationIdentifier="$5" /p:PackageVersion="$6"
+/Library/Frameworks/Mono.framework/Versions/Current/Commands/mono "/Applications/Visual Studio.app/Contents/MonoBundle/MSBuild/Current/bin/MSBuild.dll" /t:Build "$1" /p:Configuration="$2" /p:Platform="$3" /p:buildForSimulator=false /p:BuildIpa=true /p:CodesignProvision="$4" /p:ApplicationIdentifier="$5" /p:PackageVersion="$6" /bl
