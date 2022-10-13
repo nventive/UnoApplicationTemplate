28ea84bba5b4f615c842cab5c63ae03f66cfb94f
NVENTIVE\vy.nguyen <vy.nguyen@nventive.com>
2022-10-13 17:08:40 -0400

    Add tests

---
+++ src/app/ApplicationTemplate.Tests/Business/DadJokesServiceShould.cs
@@ -0,0 +1,37 @@
+﻿using System.Reactive.Subjects;
+using System.Threading;
+using System.Threading.Tasks;
+using ApplicationTemplate.Business;
+using ApplicationTemplate.Client;
+using DynamicData;
+using Moq;
+using Xunit;
+
+namespace ApplicationTemplate.Tests.Business;
+
+public partial class DadJokesServiceShould
+{
+	//private readonly Mock<IApplicationSettingsService> _applicationSettingsService;
+	//private readonly Mock<IDadJokesEndpoint> _dadJokesEndpoint;
+	//private readonly Mock<DadJokesService> _dadJokesService;
+	//private SourceList<DadJokesQuote> _favouriteQuotes;
+	//private ReplaySubject<PostTypes> _postType;
+
+	//public DadJokesServiceShould()
+	//{
+	//	_applicationSettingsService = new Mock<IApplicationSettingsService>();
+	//	_dadJokesEndpoint = new Mock<IDadJokesEndpoint>();
+	//	_dadJokesService = new Mock<DadJokesService>();
+	//	_postType = new ReplaySubject<PostTypes>(1);
+	//	_postType.OnNext(PostTypes.Hot);
+	//}
+
+	[Fact]
+	public async Task GetAllData()
+	{
+		var mockedDadJokesEndpoint = new Mock<IDadJokesEndpoint>();
+		mockedDadJokesEndpoint
+			.Setup(endpoint => endpoint.FetchData(It.IsAny<CancellationToken>(), "hot"))
+			.ReturnsAsync();
+	}
+}
