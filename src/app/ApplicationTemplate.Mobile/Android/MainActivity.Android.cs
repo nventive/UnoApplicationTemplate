fca121b5afc6e766ee79ee591cc657999f1eddec
Thomas Labrecque <thomas.labrecque@nventive.com>
2022-12-20 10:00:01 -0500

    fix: PR comment

--- src/app/ApplicationTemplate.Mobile/Android/MainActivity.Android.cs
+++ src/app/ApplicationTemplate.Mobile/Android/MainActivity.Android.cs
@@ -5,10 +5,6 @@ using Android.Views;
 namespace ApplicationTemplate;
 
 [Activity(
-	// We need to set Name, otherwise a random one is generated at every build, which causes the app to be removed from the home screen on app-updates.
-	// The name must be the fully qualified name of the class, with the namespace part in lowercase.
-	// https://developer.xamarin.com/releases/android/xamarin.android_5/xamarin.android_5.1/#Android_Callable_Wrapper_Naming
-	Name = "applicationtemplate.MainActivity",
 	MainLauncher = true,
 	ConfigurationChanges = global::Uno.UI.ActivityHelper.AllConfigChanges,
 	WindowSoftInputMode = SoftInput.AdjustNothing | SoftInput.StateHidden,
