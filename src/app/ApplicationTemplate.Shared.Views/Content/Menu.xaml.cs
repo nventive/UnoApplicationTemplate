2900a02d87eaf2156190a8c35dd6b943e532c950
Kevin Takla <kevin.takla@nventive.com>
2023-04-14 11:16:10 -0400

    fix: remove rows in shell to improve menu animation

--- src/app/ApplicationTemplate.Shared.Views/Content/Menu.xaml.cs
+++ src/app/ApplicationTemplate.Shared.Views/Content/Menu.xaml.cs
@@ -1,9 +1,37 @@
-﻿namespace ApplicationTemplate.Views.Content;
+﻿using System;
+using Microsoft.UI.Xaml;
+using Windows.UI.ViewManagement;
+
+namespace ApplicationTemplate.Views.Content;
 
 public sealed partial class Menu : AttachableUserControl
 {
 	public Menu()
 	{
 		this.InitializeComponent();
+
+		InitializeSafeArea();
+	}
+
+	private void InitializeSafeArea()
+	{
+		var bottomPadding = GetSafeAreaBottomPadding();
+
+		if (bottomPadding > 0)
+		{
+			SafeAreaRow.Height = new GridLength(bottomPadding);
+		}
+	}
+
+	private double GetSafeAreaBottomPadding()
+	{
+#if __MOBILE__
+		var bounds = App.Instance!.CurrentWindow!.Bounds;
+		var visibleBounds = ApplicationView.GetForCurrentView().VisibleBounds;
+		var safeAreaBottomPadding = bounds.Bottom - visibleBounds.Bottom;
+		return safeAreaBottomPadding;
+#else
+		return 0;
+#endif
 	}
 }
