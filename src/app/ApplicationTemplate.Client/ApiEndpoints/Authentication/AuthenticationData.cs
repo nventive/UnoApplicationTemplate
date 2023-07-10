3be0ddd4efdd6288b94ae44cc76e0cf952165ed1
Marc-Antoine Therrien <ma.therrien@nventive.com>
2023-07-10 14:54:00 -0400

    refactor: fix comments

--- src/app/ApplicationTemplate.Client/ApiEndpoints/Authentication/AuthenticationData.cs
+++ src/app/ApplicationTemplate.Client/ApiEndpoints/Authentication/AuthenticationData.cs
@@ -6,8 +6,6 @@ namespace ApplicationTemplate.Client;
 
 public class AuthenticationData : IAuthenticationToken
 {
-	public AuthenticationData() { }
-
 	public AuthenticationData(
 		string accessToken = default,
 		string refreshToken = default,
@@ -22,12 +20,12 @@ public class AuthenticationData : IAuthenticationToken
 	public AuthenticationToken AccessTokenPayload => AccessToken == null ? null : new JwtData<AuthenticationToken>(AccessToken).Payload;
 
 	[JsonPropertyName("access_token")]
-	public string AccessToken { get; set; }
+	public string AccessToken { get; init; }
 
 	[JsonPropertyName("refresh_token")]
-	public string RefreshToken { get; set; }
+	public string RefreshToken { get; init; }
 
-	public DateTimeOffset Expiration { get; set; }
+	public DateTimeOffset Expiration { get; init; }
 
 	public string Email => AccessTokenPayload?.Email;
 
