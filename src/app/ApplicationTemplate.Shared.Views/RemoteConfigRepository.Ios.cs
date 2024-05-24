78f04c14094e4c2725920b463ee1d0990c75992b
Marc-Antoine Soucy <ma.soucy@nventive.com>
2024-05-24 10:55:51 -0400

    a

---
+++ src/app/ApplicationTemplate.Shared.Views/RemoteConfigRepository.Ios.cs
@@ -0,0 +1,12 @@
+﻿#if __IOS__
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationTemplate.Views
+{
+	internal class RemoteConfigRepository
+	{
+	}
+}
+#endif
