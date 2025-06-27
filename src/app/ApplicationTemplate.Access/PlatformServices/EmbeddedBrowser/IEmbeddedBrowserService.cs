8c410a9c021fdf96f26ec2de3d62320fc9e31a4d
Thomas Labrecque <thomas.labrecque@nventive.com>
2025-06-27 13:52:20 -0400

    f

---
+++ src/app/ApplicationTemplate.Access/PlatformServices/EmbeddedBrowser/IEmbeddedBrowserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CPS.DataAccess;
+
+/// <summary>
+/// Defines a contract dedicated to launching the embedded browser.
+/// </summary>
+public interface IEmbeddedBrowserService
+{
+	/// <summary>
+	/// This function call the browser with an uri.
+	/// </summary>
+	/// <param name="ct">Cancellation Token.</param>
+	/// <param name="uri">The <see cref="Uri"/>.</param>
+	Task NavigateTo(CancellationToken ct, Uri uri);
+}
