2575584363d90335c6902fcbdb7690f7300760cf
Thomas Labrecque <thomas.labrecque@nventive.com>
2025-07-02 09:31:22 -0400

    raven

---
+++ src/app/ApplicationTemplate.Shared.Views/PlatformServices/NetworkType/NetworkTypeProvider.Android.cs
@@ -0,0 +1,44 @@
+﻿// src/app/ApplicationTemplate.Shared.Views/PlatformServices/NetworkType/NetworkTypeProvider.Android.cs
+#if __ANDROID__
+using Android.Net;
+
+namespace ApplicationTemplate.DataAccess.PlatformServices;
+
+/// 
+/// The Android implementation of .
+/// 
+public sealed class NetworkTypeProvider : INetworkTypeProvider
+{
+	/// 
+	public NetworkType NetworkType => GetNetworkType();
+
+	private static NetworkType GetNetworkType()
+	{
+		var context = Android.App.Application.Context;
+		var cm = (ConnectivityManager)context.GetSystemService(Android.Content.Context.ConnectivityService);
+		var activeNetwork = cm?.ActiveNetwork;
+		if (activeNetwork == null)
+		{
+			return NetworkType.None;
+		}
+		var capabilities = cm.GetNetworkCapabilities(activeNetwork);
+		if (capabilities == null)
+		{
+			return NetworkType.None;
+		}
+		if (capabilities.HasTransport(Transport.Wifi))
+		{
+			return NetworkType.Wifi;
+		}
+		if (capabilities.HasTransport(Transport.Ethernet))
+		{
+			return NetworkType.Ethernet;
+		}
+		if (capabilities.HasTransport(Transport.Cellular))
+		{
+			return NetworkType.Cellular;
+		}
+		return NetworkType.Unknown;
+	}
+}
+#endif
---
+++ src/app/ApplicationTemplate.Shared.Views/PlatformServices/NetworkType/NetworkTypeProvider.iOS.cs
@@ -0,0 +1,37 @@
+﻿// src/app/ApplicationTemplate.Shared.Views/PlatformServices/NetworkType/NetworkTypeProvider.iOS.cs
+#if __IOS__
+using SystemConfiguration;
+
+namespace ApplicationTemplate.DataAccess.PlatformServices;
+
+/// 
+/// The iOS implementation of .
+/// 
+public sealed class NetworkTypeProvider : INetworkTypeProvider
+{
+	/// 
+	public NetworkType NetworkType
+	{
+		get
+		{
+			var reachability = new SCNetworkReachability(new System.Net.IPAddress(0));
+			SCNetworkReachabilityFlags flags;
+			if (!reachability.TryGetFlags(out flags))
+			{
+				return NetworkType.None;
+			}
+			if (!flags.HasFlag(SCNetworkReachabilityFlags.Reachable))
+			{
+				return NetworkType.None;
+			}
+			var isWWAN = flags.HasFlag(SCNetworkReachabilityFlags.IsWWAN);
+			if (isWWAN)
+			{
+				return NetworkType.Cellular;
+			}
+			// Assuming WiFi if not WWAN and reachable, as Ethernet is rare on iOS.
+			return NetworkType.Wifi;
+		}
+	}
+}
+#endif
