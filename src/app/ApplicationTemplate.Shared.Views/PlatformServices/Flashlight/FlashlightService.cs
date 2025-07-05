1f737df99d0c95189056ad5bc8cec1fd14667ca1
Thomas Labrecque <thomas.labrecque@nventive.com>
2025-07-05 14:21:40 -0400

    beetle_postedit

---
+++ src/app/ApplicationTemplate.Shared.Views/PlatformServices/Flashlight/FlashlightService.cs
@@ -0,0 +1,32 @@
+﻿// path/to/FlashlightService.cs
+using Windows.Devices.Lights;
+
+namespace ApplicationTemplate.DataAccess.PlatformServices
+{
+	public class FlashlightService : IFlashlightService
+	{
+		private Lamp _lamp;
+
+		public float Brightness { get; set; } = 1.0f;
+
+		public async Task Initialize()
+		{
+			_lamp = await Lamp.GetDefault();
+		}
+
+		public void Toggle()
+		{
+			if (_lamp != null)
+			{
+				if (_lamp.IsOn)
+				{
+					_lamp.Disable();
+				}
+				else
+				{
+					_lamp.Enable(Brightness);
+				}
+			}
+		}
+	}
+}
