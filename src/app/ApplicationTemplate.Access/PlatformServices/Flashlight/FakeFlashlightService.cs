ba3d9d958ba00d05c36442ffa70bd6c61ae85cf7
Thomas Labrecque <thomas.labrecque@nventive.com>
2025-07-05 12:10:59 -0400

    rewrite

---
+++ src/app/ApplicationTemplate.Access/PlatformServices/Flashlight/FakeFlashlightService.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace ApplicationTemplate.DataAccess.PlatformServices;
+
+/// <summary>
+/// The fake <see cref="IFlashlightService"/> implementation for testing purposes.
+/// </summary>
+public sealed class FakeFlashlightService : IFlashlightService
+{
+	/// <inheritdoc/>
+	public float Brightness { get; set; }
+
+	/// <inheritdoc/>
+	public Task Initialize()
+	{
+		return Task.CompletedTask;
+	}
+
+	/// <inheritdoc/>
+	public void Toggle()
+	{
+		return;
+	}
+}
---
+++ src/app/ApplicationTemplate.Access/PlatformServices/Flashlight/IFlashlightService.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+
+namespace ApplicationTemplate.DataAccess.PlatformServices;
+
+/// <summary>
+/// Provides access to the device's flashlight functionality.
+/// </summary>
+public interface IFlashlightService
+{
+	/// <summary>
+	/// Gets or sets the brightness of the flashlight.
+	/// </summary>
+	/// <remarks>
+	/// On Android, flashlight brightness cannot be controlled, hence any non-zero BrightnessLevel results in the full brightness of the flashlight.
+	/// On iOS, in case the device supports the torch, BrightnessLevel is fully supported. In case the device has only flash, any non-zero BrightnessLevel will result in the full brightness of the flashlight.
+	/// </remarks>
+	float Brightness { get; set; }
+
+	/// <summary>
+	/// Initializes the flashlight service.
+	/// </summary>
+	Task Initialize();
+
+	/// <summary>
+	/// Toggles the flashlight state on or off.
+	/// </summary>
+	/// <remarks>
+	/// You must set the <see cref="Brightness"/> property before or after calling this method to ensure the desired brightness level is applied when the flashlight is turned on.
+	/// </remarks>
+	void Toggle();
+}
