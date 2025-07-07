3156d181672727d64e66a6aea0302741e7e2317b
Thomas Labrecque <thomas.labrecque@nventive.com>
2025-07-07 07:36:14 -0400

    preedit

---
+++ src/app/ApplicationTemplate.Access/PlatformServices/ScreenWakeLock/FakeScreenWakeLockService.cs
@@ -0,0 +1,19 @@
+﻿namespace ApplicationTemplate.DataAccess.PlatformServices;
+
+/// <summary>
+/// Fake implementation of <see cref="IScreenWakeLockService"/> for testing purposes.
+/// </summary>
+public sealed class FakeScreenWakeLockService : IScreenWakeLockService
+{
+	/// <inheritdoc/>
+	public void Disable()
+	{
+		return;
+	}
+
+	/// <inheritdoc/>
+	public void Enable()
+	{
+		return;
+	}
+}
---
+++ src/app/ApplicationTemplate.Access/PlatformServices/ScreenWakeLock/IScreenWakeLockService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ApplicationTemplate.DataAccess.PlatformServices;
+
+/// <summary>
+/// Provides a way for keeping the device's display on.
+/// </summary>
+public interface IScreenWakeLockService
+{
+	/// <summary>
+	/// Enables the device's keep screen on feature.
+	/// </summary>
+	void Enable();
+
+	/// <summary>
+	/// Disables the device's keep screen on feature.
+	/// </summary>
+	/// <remarks>
+	/// Throws <see cref="Exception"/> if no request has been made.
+	/// </remarks>
+	void Disable();
+}
