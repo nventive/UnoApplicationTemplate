75bf5c4fc3279355946257c267f0f5060ce12af8
Thomas Labrecque <thomas.labrecque@nventive.com>
2025-07-08 09:36:03 -0400

    beetle_postedit

--- src/app/ApplicationTemplate.Shared.Views/PlatformServices/BatteryInformation/BatteryInformationProvider.cs
+++ src/app/ApplicationTemplate.Shared.Views/PlatformServices/BatteryInformation/BatteryInformationProvider.cs
@@ -1,95 +1,62 @@
-﻿using System;
+﻿// src/app/ApplicationTemplate.Shared.Views/Configuration/BatteryInformationProvider.cs
+using System;
 using System.Reactive.Linq;
-using Windows.System.Power;
 
-namespace ApplicationTemplate.DataAccess.PlatformServices;
-
-/// <summary>
-/// The <see cref="IBatteryInformationProvider"/> implementation using Uno.
-/// </summary>
-public sealed class BatteryInformationProvider : IBatteryInformationProvider
+namespace ApplicationTemplate.DataAccess.PlatformServices
 {
-	/// <inheritdoc/>
-	public BatteryStatus BatteryStatus => PowerManager.BatteryStatus.ToInternalBatteryStatus();
-
-	/// <inheritdoc/>
-	public EnergySaverStatus EnergySaverStatus => PowerManager.EnergySaverStatus.ToInternalEnergySaverStatus();
-
-	/// <inheritdoc/>
-	public PowerSupplyStatus PowerSupplyStatus => PowerManager.PowerSupplyStatus.ToInternalPowerSupplyStatus();
-
-	/// <inheritdoc/>
-	public int RemainingChargePercent => PowerManager.RemainingChargePercent;
-
-	/// <inheritdoc/>
-	public TimeSpan RemainingDischargeTime
-#if __WINDOWS__
-		=> PowerManager.RemainingDischargeTime;
-#else
-		=> TimeSpan.Zero;
-#endif
-
-	/// <inheritdoc/>
-	public IObservable<BatteryStatus> GetAndObserveBatteryStatus()
+	/// 
+	/// Implementation of IBatteryInformationProvider using Uno Platform.
+	/// 
+	public class BatteryInformationProvider : IBatteryInformationProvider
 	{
-		return Observable.FromEventPattern<EventHandler<object>, object>(
-			h => PowerManager.BatteryStatusChanged += h,
-			h => PowerManager.BatteryStatusChanged -= h
-		)
-		.Select(_ => BatteryStatus)
-		.StartWith(BatteryStatus)
-		.DistinctUntilChanged();
-	}
+		public BatteryStatus BatteryStatus => Windows.System.Power.Battery.Report.Status;
 
-	/// <inheritdoc/>
-	public IObservable<EnergySaverStatus> GetAndObserveEnergySaverStatus()
-	{
-		return Observable.FromEventPattern<EventHandler<object>, object>(
-			h => PowerManager.EnergySaverStatusChanged += h,
-			h => PowerManager.EnergySaverStatusChanged -= h
-		)
-		.Select(_ => EnergySaverStatus)
-		.StartWith(EnergySaverStatus)
-		.DistinctUntilChanged();
-	}
+		public EnergySaverStatus EnergySaverStatus => Windows.System.Power.EnergySaverInfo.GetCurrentStatus();
 
-	/// <inheritdoc/>
-	public IObservable<PowerSupplyStatus> GetAndObservePowerSupplyStatus()
-	{
-		return Observable.FromEventPattern<EventHandler<object>, object>(
-			h => PowerManager.PowerSupplyStatusChanged += h,
-			h => PowerManager.PowerSupplyStatusChanged -= h
-		)
-		.Select(_ => PowerSupplyStatus)
-		.StartWith(PowerSupplyStatus)
-		.DistinctUntilChanged();
-	}
+		public PowerSupplyStatus PowerSupplyStatus => Windows.System.Power.Battery.Report.PowerSupplyStatus;
 
-	/// <inheritdoc/>
-	public IObservable<int> GetAndObserveRemainingChargePercent()
-	{
-		return Observable.FromEventPattern<EventHandler<object>, object>(
-			h => PowerManager.RemainingChargePercentChanged += h,
-			h => PowerManager.RemainingChargePercentChanged -= h
-		)
-		.Select(_ => RemainingChargePercent)
-		.StartWith(RemainingChargePercent)
-		.DistinctUntilChanged();
-	}
+		public int RemainingChargePercent => Windows.System.Power.Battery.Report.RemainingCapacityPercent;
 
-	/// <inheritdoc/>
-	public IObservable<TimeSpan> GetAndObserveRemainingDischargeTime()
-	{
-#if __WINDOWS__
-		return Observable.FromEventPattern<EventHandler<object>, object>(
-			h => PowerManager.RemainingDischargeTimeChanged += h,
-			h => PowerManager.RemainingDischargeTimeChanged -= h
-		)
-		.Select(_ => RemainingDischargeTime)
-		.StartWith(RemainingDischargeTime)
-		.DistinctUntilChanged();
-#else
-		return Observable.Return(RemainingDischargeTime);
-#endif
+		public TimeSpan RemainingDischargeTime => Windows.System.Power.Battery.Report.RemainingDischargeTime;
+
+		public IObservable GetAndObserveBatteryStatus()
+		{
+			return Observable.FromEventPattern(
+				h => Windows.System.Power.Battery.ReportStatusChanged += h,
+				h => Windows.System.Power.Battery.ReportStatusChanged -= h
+			).Select(_ => Windows.System.Power.Battery.Report.Status);
+		}
+
+		public IObservable GetAndObserveEnergySaverStatus()
+		{
+			return Observable.FromEventPattern(
+				h => Windows.System.Power.EnergySaverInfo.StatusChanged += h,
+				h => Windows.System.Power.EnergySaverInfo.StatusChanged -= h
+			).Select(_ => Windows.System.Power.EnergySaverInfo.GetCurrentStatus());
+		}
+
+		public IObservable GetAndObservePowerSupplyStatus()
+		{
+			return Observable.FromEventPattern(
+				h => Windows.System.Power.Battery.ReportStatusChanged += h,
+				h => Windows.System.Power.Battery.ReportStatusChanged -= h
+			).Select(_ => Windows.System.Power.Battery.Report.PowerSupplyStatus);
+		}
+
+		public IObservable GetAndObserveRemainingChargePercent()
+		{
+			return Observable.FromEventPattern(
+				h => Windows.System.Power.Battery.ReportStatusChanged += h,
+				h => Windows.System.Power.Battery.ReportStatusChanged -= h
+			).Select(_ => Windows.System.Power.Battery.Report.RemainingCapacityPercent);
+		}
+
+		public IObservable GetAndObserveRemainingDischargeTime()
+		{
+			return Observable.FromEventPattern(
+				h => Windows.System.Power.Battery.ReportStatusChanged += h,
+				h => Windows.System.Power.Battery.ReportStatusChanged -= h
+			).Select(_ => Windows.System.Power.Battery.Report.RemainingDischargeTime);
+		}
 	}
 }
