0567c010e7ce0a7ca5532fb87b501749a2ff50de
Thomas Labrecque <thomas.labrecque@nventive.com>
2025-07-10 07:41:45 -0400

    preedit

---
+++ src/app/ApplicationTemplate.Access/LocalStorage/FakeCredentialsRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+namespace ApplicationTemplate.DataAccess.LocalStorage;
+
+/// <inheritdoc/>
+public sealed class FakeCredentialsRepository : ICredentialsRepository
+{
+	/// <inheritdoc/>
+	public Task<Credentials> Read(string resource, string username)
+	{
+		return Task.FromResult(new Credentials
+		{
+			Username = username,
+			Password = "fake-password-for-" + username,
+		});
+	}
+
+	/// <inheritdoc/>
+	public Task Write(string resource, Credentials credentials)
+	{
+		return Task.CompletedTask;
+	}
+}
