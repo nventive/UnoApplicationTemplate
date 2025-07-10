0c86689dbb2690547f019911372c7685c14af0df
Thomas Labrecque <thomas.labrecque@nventive.com>
2025-07-10 07:27:17 -0400

    preedit

---
+++ src/app/ApplicationTemplate.Access/LocalStorage/Credentials.cs
@@ -0,0 +1,8 @@
+﻿namespace ApplicationTemplate.DataAccess.LocalStorage;
+
+public sealed class Credentials
+{
+	public string Username { get; init; }
+
+	public string Password { get; init; }
+}
---
+++ src/app/ApplicationTemplate.Access/LocalStorage/ICredentialsRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationTemplate.DataAccess.LocalStorage;
+
+/// <summary>
+/// Provides access to the local credentials secure storage.
+/// </summary>
+public interface ICredentialsRepository
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="resource"></param>
+	/// <param name="username"></param>
+	/// <returns></returns>
+	Task<Credentials> Read(string resource, string username);
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="resource"></param>
+	/// <param name="credentials"></param>
+	Task Write(string resource, Credentials credentials);
+}
